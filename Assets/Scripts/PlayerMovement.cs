51c80820fbf81b30876235d211bc34a4332b36d3
pabloarak <pablo.arak.vm@gmail.com>
2020-10-12 17:04:35 -0300

    Added Cinemachine2D. Fix jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour {
     public float speed = 2f;
     public float jumpSpeed = 300;
 
+    bool isGrounded = true;
+
     // Start is called before the first frame update
     void Start() {
         
@@ -17,8 +19,17 @@ public class PlayerMovement : MonoBehaviour {
     void Update() {
         playerRb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, playerRb.velocity.y);
 
-        if(Input.GetKeyDown(KeyCode.Space)){
-            playerRb.AddForce(Vector2.up * jumpSpeed);
+        if(isGrounded) {
+            if(Input.GetKeyDown(KeyCode.Space)){
+                playerRb.AddForce(Vector2.up * jumpSpeed);
+                isGrounded = false;
+            }
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision) { // collision=todo lo que choca con nuestro pj
+        if(collision.gameObject.CompareTag("Ground")) {
+            isGrounded = true;
         }
     }
 }
